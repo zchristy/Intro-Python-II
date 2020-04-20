3a9cefea82e3dd593e40b1449f0fe76d98973739
zchristy <zchristy44@gmail.com>
2020-04-20 04:49:26 -0500

    Finishing up first half of project
    Player can move to different rooms, ask for help, and quit the game. Messages have been put in place along the way

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -23,7 +24,6 @@ earlier adventurers. The only exit is to the south."""),
 
 
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -38,6 +38,17 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player('player', room['outside'])
+
+# room property to check if valid movement
+def try_movement(direction, room):
+    attribute = direction + '_to'
+
+    if hasattr(room, attribute):
+        return getattr(room, attribute)
+    else:
+        print(f'\nLooks like a dead end.... try another direction.\n')
+        return room
 
 # Write a loop that:
 #
@@ -49,3 +60,63 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+quit = False
+helping = False
+
+commands = ['n', 'e', 's', 'w']
+controls = ['q', 'help']
+
+help_message = '========== DIRECTIONS ===========\n\nUse the keys:  n | e | s | w - to move from room to room.\nUse the key:  q - to quit game.\n\n================================='
+
+error_message = '\nThinking outside the box, I like it!\nPlease stick to n, e, s, w, or q.\nEnter \'help\' for more details.\n'
+
+help_error = '\n---> Invalid input. Please type \'y\' or \'n\'. <---\n'
+
+help_y = '\n---> Glad to be of service! <---'
+help_n = '\n---> Take your time. <---\n'
+
+while not quit:
+
+    while helping:
+        # help prompt for user input
+        help_prompt = input('Done? [y] [n]: ')
+
+        if help_prompt == 'y':
+            helping = False
+            print(help_y)
+        elif help_prompt == 'n':
+            helping = True
+            print(help_n)
+            print(help_message)
+        else:
+            helping = True
+            print(help_error)
+            print(help_message)
+
+    # print players current room and description
+    print(f'\n...{player.current_room.name}: {player.current_room.description}\n')
+    print(f'\n...Inventory: {player.inventory}\n')
+    print(f'\n...Items in Room: {player.current_room.items}\n')
+
+    # User Input
+    prompt = input('---> ')
+    pt = prompt[0]
+
+    # quit game if player inputs 'q'
+    if pt == 'q':
+        quit = True
+
+    # Directions
+    if prompt == 'help':
+        helping = True
+        print(help_message)
+
+    # Catch non valid user inputs
+    if pt not in commands and prompt not in controls:
+        print(error_message)
+
+    # Valid user inputs
+    if pt in commands:
+        print(f'\nChecking route....\n')
+        player.current_room = try_movement(pt, player.current_room)
--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player():
+    def __init__(self, name, current_room, inventory=[]):
+        self.name = name
+        self.current_room = current_room
+        self.inventory = inventory
+
+    def __str__(self):
+        return 'Name: {self.name}, C-Room: {self.current_room}'.format(self=self)
